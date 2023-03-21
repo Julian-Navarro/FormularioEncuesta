eee6d3557a4c787896d5c85ee12dcae8be011055
Julian Navarro <julianrodrigonavarro@gmail.com>
2023-03-21 16:13:13 -0300

    Adding middlewares

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,11 +2,15 @@ import "reflect-metadata";
 import express from "express";
 import RouterController from "./router/index";
 import database from "./config/database";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 database.initialize()
 .then(()=> console.log("Database connected"))
 .catch((error)=>console.log(error)
