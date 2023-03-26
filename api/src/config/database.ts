35e671773406a3be420ec8e45181d1f14156d635
Julian Navarro <julianrodrigonavarro@gmail.com>
2023-03-26 16:50:45 -0300

    Deploy config

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -1,13 +1,19 @@
 import { DataSource } from "typeorm";
 import { Survey } from "../entity/Survey";
+import { 
+DB_PASSWORD,
+DB_HOST,
+DB_NAME,
+DB_PORT
+} from "./config"
 
 export default new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
+    host: DB_HOST,
+    port: Number(DB_PORT),
     username:"postgres",
-    password: "BVBadmin",
-    database:"surveyForm",
+    password: DB_PASSWORD,
+    database: DB_NAME,
     entities: [Survey],
     synchronize: true,
     logging: false
